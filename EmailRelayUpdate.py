1d8841c394477f50d9d6716b15116ec00850ada5
Jordon <epathickernel@gmail.com>
2017-08-27 16:48:18 -0400

    Changing the way of DNS resolving. Need to test on Linux.

--- EmailRelayUpdate.py
+++ EmailRelayUpdate.py
@@ -1,14 +1,25 @@
 #This script will find the new IPs of domains to update the PostFix relay setting with
 import socket
-import dns.resolver
-
+#import dns.resolver
+import subprocess
+import shlex
 
 
 #Dyn Domain -- will change this to find the domain via some method that doesn't get over wrote via Git
 domain = "'remote.imaqeo.com'"
 
-IPGrab(domain)
+
+#Let's scrap this way of doing it and find a simpler way
+
+#def IPGrab(domain):
+#    for rdata in dns.resolver.query( domain, 'CNAME') :
+#        print(rdata.target)
 
 def IPGrab(domain):
-    for rdata in dns.resolver.query( domain, 'CNAME') :
-        print(rdata.target)
+    cmd = "dig +short domain | sed -n 2p"
+    proc=subprocess.Popen(shlex.split(cmd),stdout=subprocess.PIPE)
+    out,err=proc.communicate()
+    print(out)
+
+
+IPGrab(domain)
