a133d153d75d6ae1102f00e98814da75d1046676
Jordon <epathickernel@gmail.com>
2017-10-08 11:13:00 -0400

    Added ability to change domain from settings file Added meaningful comments

--- EmailRelayUpdate.py
+++ EmailRelayUpdate.py
@@ -6,33 +6,48 @@ import shlex
 import re
 import time
 
-#Dyn Domain -- will change this to find the domain via some method that doesn't get overwrote via Git
-domain = "'remote.imaqeo.com'"
-path = "/etc/postfix/main.cf"
+#Initilizing the globals
+#
+#'domain' is the domain that we set in the settings.config file. This is the dynamic domain we are querying
+#'path' is the path to the PostFix main.cf file. This is read from the settings.config file
+domain = ""
+path = ""
 
 
 
 #Bash command function
+#
+#INPUT: String
+#INPUT is a command that is to be ran in bash
+#OUTPUT: String
+#OUTPUT is the result of INPUT in a String format
 def excomm(com):
     proc=subprocess.Popen(shlex.split(com),stdout=subprocess.PIPE)
     out,err=proc.communicate()
     output=out.decode("utf-8")
     return output
-    #We need to do something with any errors
 
 #Grab the IP from a dig
-def IPGrab(dom):
-    decode = excomm("dig +short %s" % dom)
-    ErrorHandler("Dig result of %s" % dom + " is: %s" % decode)
-    ips = re.findall( '\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b', decode)
+#
+#INPUT: NONE
+#OUTPUT: String
+#OUTPUT is the string of IPs that is returned from the DIG
+def IPGrab():
+    decode = excomm("dig +short %s" % domain)
+    ErrorHandler("Dig result of %s" % domain + " is: %s" % decode) #Need to write this to the error log in case anything is captured. Also helps in troubleshooting
+    ips = re.findall( '\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b', decode) #We're only looking for IPs
     return ips
 
+#Main
+#INPUT: NONE
+#OUTPUT: NONE
+#This is the main function that also modifies the main.cf file
 def FileEditor():
     ipstring = ""
-    ips=IPGrab(domain)
+    ips=IPGrab()
     if len(ips) == 1:
-        ipstring += ips[0] + "/32"
-    else:
+        ipstring += ips[0] + "/32" #Don't want a comma if it's only one IP
+    else: #Otherwise we go through and add commas until the last IP
         c = 1
         for IP in ips:
             if c < len(ips):
@@ -42,40 +57,55 @@ def FileEditor():
                 ipstring += IP + "/32"
                 c += 1
         
-    ErrorHandler("New IP string is %s" % ipstring)
-    #Let's create the temp file
-    temp = open("/tmp/emailrelay.tmp", "a")
-    #Let's open the current file
+    ErrorHandler("New IP string is %s" % ipstring) #Need to write this to error log for troubleshooting
+
+    temp = open("/tmp/emailrelay.tmp", "a") # Creating a temp file so the main.cf file has less chance to disappear
+
     with open (path, 'rt') as in_file:
         #Now write each line individually to the new file. Looking for the 'mynetworks = ' line
         for line in in_file:
             if line.find("mynetworks =")==0:
-                if line.find("#")!=0:
+                if line.find("#")!=0: #Don't want commented lines
                     temp.write("mynetworks = %s\n" % ipstring)
             else:    
                 temp.write(line)
 
-    temp.close()
+    temp.close() #Close the temp file
 
-    e1 = excomm("mv -f /tmp/emailrelay.tmp %s" % path)
+    #Now lets start cleaning house with our temp file
+    e1 = excomm("mv -f /tmp/emailrelay.tmp %s" % path) #Let's overwrite the main.cf file with our temp file
     if e1 == "":
-        ErrorHandler("No error during overwrite of temp file to %s" % path)
+        ErrorHandler("No error during overwrite of temp file to %s" % path) #There are no errors but need troubleshoot tracking
     else:
-        ErrorHandler(e1)
-    #print out to error log
-    e2 = excomm("rm -rf /tmp/emailrealy.tmp")
-    #print out to error log
+        ErrorHandler(e1) #There is an error so lets log it
+    e2 = excomm("rm -rf /tmp/emailrealy.tmp") #Let's remove our temp file
     if e2 == "":
-        ErrorHandler("No error doing removal of /tmp/emailrealy.tmp")
+        ErrorHandler("No error doing removal of /tmp/emailrealy.tmp") #There are no errors but need troubleshoot tracking
     else:
-        ErrorHandler(e2)
-    #for IP in IPGrab(domain):
-     #   print(IP + "/32")
+        ErrorHandler(e2) #There is an error so lets log it
+
+#Error Handling
+#INPUT: String
+#INPUT is the string to write to the error log
+#OUTPUT: NONE
+#Errors are written to /var/log/EmailRelayUpdate.log
 def ErrorHandler(err):
     ef = open("/var/log/EmailRelayUpdate.log", "a")
-    #error = datetime.datetime + " -- " + err
-    ef.write(time.strftime("%m/%d/%Y:%H:%M:%S") + " -- " + err + "\n")
+    ef.write(time.strftime("%m/%d/%Y:%H:%M:%S") + " -- " + err + "\n") #Let's write the error
     ef.close()
 
+#Grabing of the globals
+#INPUT: NONE
+#OUTPUT: NONE
+def SetConfig():
+    with open ("./settings.config", 'rt') as config:
+        for line in config:
+            if line.find("#")!=0:
+                if line.find("DOMAIN:")==0:
+                    domain = line[7:]
+                if line.find("POSTFIX:")==0:
+                    path = line[8:]
 
+
+SetConfig()
 FileEditor()
